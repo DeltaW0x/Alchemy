537d04a882d54ab31a0898e7d6eee5f214b8b5d8
Delta <98419797+DeltaW0x@users.noreply.github.com>
2024-05-28 13:24:47 +0200

    Moved from executable to static library

---
+++ Source/Alchemy.h
@@ -0,0 +1,18 @@
+//
+// Created by Luca Cassarino on 28/05/24.
+//
+
+#ifndef ALCHEMY_H
+#define ALCHEMY_H
+
+#include "Window/Window.h"
+#include "Renderer/Renderer.h"
+#include "Miscellaneous/Types.h"
+#include "Math/Float2.h"
+#include "Math/Float3.h"
+#include "Math/Float4.h"
+#include "Math/MatrixMath.h"
+#include "ECS/Transform.h"
+#include "ECS/Rendering/StaticMesh.h"
+
+#endif //ALCHEMY_H
